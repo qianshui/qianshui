af1c5012677f1739bebe24a65f5faeaf0ca77fb8
狼哥 <jswxjiangnanhong@163.com>
2014-03-21 11:14:52 +0800

    add flow

--- SourceCode/Server/WebService/src/DataBase/Flow.java
+++ SourceCode/Server/WebService/src/DataBase/Flow.java
@@ -2,112 +2,111 @@ package DataBase;
 
 import java.util.Date;
 
-
 /**
  * Flow entity. @author MyEclipse Persistence Tools
  */
 
-public class Flow  implements java.io.Serializable {
+public class Flow implements java.io.Serializable {
 
+	// Fields
 
-    // Fields    
+	private String id;
+	private Subject subject;
+	private String title;
+	private String description;
+	private Date addTime;
+	private String submitter;
+	private String type;
+	private String categoryId;
 
-     private String id;
-     private Subject subject;
-     private String title;
-     private String description;
-     private Date addTime;
-     private String submitter;
-     private String type;
+	// Constructors
 
-
-    // Constructors
-
-    /** default constructor */
-    public Flow() {
-    }
+	/** default constructor */
+	public Flow() {
+	}
 
 	/** minimal constructor */
-    public Flow(String id) {
-        this.id = id;
-    }
-    
-    /** full constructor */
-    public Flow(String id, Subject subject, String title, String description, Date addTime, String submitter, String type) {
-        this.id = id;
-        this.subject = subject;
-        this.title = title;
-        this.description = description;
-        this.addTime = addTime;
-        this.submitter = submitter;
-        this.type = type;
-    }
+	public Flow(String id) {
+		this.id = id;
+	}
 
-   
-    // Property accessors
+	/** full constructor */
+	public Flow(String id, Subject subject, String title, String description,
+			Date addTime, String submitter, String type, String categoryId) {
+		this.id = id;
+		this.subject = subject;
+		this.title = title;
+		this.description = description;
+		this.addTime = addTime;
+		this.submitter = submitter;
+		this.type = type;
+		this.categoryId = categoryId;
+	}
 
-    public String getId() {
-        return this.id;
-    }
-    
-    public void setId(String id) {
-        this.id = id;
-    }
+	// Property accessors
 
-    public Subject getSubject() {
-        return this.subject;
-    }
-    
-    public void setSubject(Subject subject) {
-        this.subject = subject;
-    }
+	public String getId() {
+		return this.id;
+	}
 
-    public String getTitle() {
-        return this.title;
-    }
-    
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getDescription() {
-        return this.description;
-    }
-    
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public Subject getSubject() {
+		return this.subject;
+	}
 
-    public Date getAddTime() {
-        return this.addTime;
-    }
-    
-    public void setAddTime(Date addTime) {
-        this.addTime = addTime;
-    }
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
 
-    public String getSubmitter() {
-        return this.submitter;
-    }
-    
-    public void setSubmitter(String submitter) {
-        this.submitter = submitter;
-    }
+	public String getTitle() {
+		return this.title;
+	}
 
-    public String getType() {
-        return this.type;
-    }
-    
-    public void setType(String type) {
-        this.type = type;
-    }
-   
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
+	public String getDescription() {
+		return this.description;
+	}
 
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
+	public Date getAddTime() {
+		return this.addTime;
+	}
 
+	public void setAddTime(Date addTime) {
+		this.addTime = addTime;
+	}
 
+	public String getSubmitter() {
+		return this.submitter;
+	}
 
+	public void setSubmitter(String submitter) {
+		this.submitter = submitter;
+	}
 
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getCategoryId() {
+		return this.categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
 
 }
