fa340d7ec44ea9ac6011018a9b0b194e7082958b
brett <whipsnade@qq.com>
2014-03-27 15:12:21 +0800

    修改DetailList bug 在laws 和policy 中添加imgid 字段

--- Client/WebRoot/app/view/query/DetailList.js
+++ Client/WebRoot/app/view/query/DetailList.js
@@ -4,10 +4,10 @@ Ext.define('YongYou.view.query.DetailList', {
 	config : {
 		layout : 'fit',
 		cls : 'ks-basic',
-		//id : 'QueryPanelDetailList',
+		// id : 'QueryPanelDetailList',
 		scrollable : true,
-		title:"",
-		type:null,
+		title : "",
+		type : null,
 		height : '100%',
 		items : [{
 			xtype : 'dataview',
@@ -15,38 +15,12 @@ Ext.define('YongYou.view.query.DetailList', {
 				direction : 'vertical'
 			},
 			cls : 'dataview-basic',
-			itemTpl : '<div class="img" style="background-image: url(\'{imgId}\')">'
+			itemTpl : '<div class="img" style="background-image: url(\'resources/img/{imgId}\')">'
 					+ '</div><div class="content"><div class="name">{title}</div>'
-					+ '<div class="affiliation">{subtitle}</div></div>',
-			//store : 
+					+ '<div class="affiliation">{subTitle}</div></div>',
 			listeners : {
 				itemtap : function(me, list, index, item, e) {
-
-					if (me.parent.getType() == "bz") {
-						YongYou.util.DataApi.Core.getBZ(showDetail, item, {
-									'ID' : item.internalId
-								})
-					} else if (me.parent.getType() == "fg") {
-						YongYou.util.DataApi.Core.getLawByLawID(showDetail, item, {
-									'id' : item.internalId
-								})
-					} else if (me.parent.getType() == "zc") {
-						YongYou.util.DataApi.Core.getPolicyByPolicyID(showDetail, item, {
-									'id' : item.internalId
-								})
-					}
-
-					// *******************************make element.dom as map
-					// target, but only first time the map will display at the
-					// center*********************
-					// win.initialPanelControl(new Ext.Panel({
-					// fullscreen : true,
-					// layout : 'fit',
-					// height : '100%',
-					// width:'100%',
-					// items : [Ext.create('YongYou.util.Map',{})]
-					// }), 'test')
-
+					showDetail(item);
 				}
 			}
 
@@ -54,24 +28,14 @@ Ext.define('YongYou.view.query.DetailList', {
 	}
 });
 
-function showDetail(res, scope) {
+function showDetail(item) {
 	var win = Ext.create('YongYou.util.Window', {});
-	if (res.length <= 5) {
-		var map = Ext.create('YongYou.util.MapPanel', {
-					id : scope.internalId
-				})
-		win.initialPanelControl(map, scope.data.title)
-		new Ext.onReady(function() {
-					map.initMap();
-				}, win)
-	} else {
-		win.initialPanelControl(new Ext.Panel({
-							fullscreen : true,
-							scrollable : true,
-							layout : 'fit',
-							height : '100%',
-							width : '100%',
-							html : res
-						}), scope.data.title)
-	}
+	win.initialPanelControl(new Ext.Panel({
+						fullscreen : true,
+						scrollable : true,
+						layout : 'fit',
+						height : '100%',
+						width : '100%',
+						html : item.data.content
+					}), item.data.title)
 }
