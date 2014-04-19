35e0f20c6648c58f48353c42f2bb2f10f66dd8ee
brett <whipsnade@qq.com>
2014-04-20 00:01:02 +0800

    修改信息确认样式

--- Client/WebRoot/app/view/InformationConfirm.js
+++ Client/WebRoot/app/view/InformationConfirm.js
@@ -9,16 +9,17 @@ Ext.define('YongYou.view.InformationConfirm', {
 					xtype : 'panel'
 				}, {
 					xtype : 'panel',
-					html : '<div style="width:800px;line-height:35px;margin:100px 1px 2px 5px;'
+					html : '<div style="width:800px;line-height:35px;margin-top:55px;'
 							+ 'text-align:left;font-size:14px;font-family: Georgia;'
-							+ 'font-weight:bold;color:rgba(171, 93, 57,0.8);">&nbsp;&nbsp;在您选取的位置方圆3公里配套情况如下：&nbsp;&nbsp;</div>'
+							+ 'font-weight:bold;color:rgba(171, 93, 57,0.8);">&nbsp;&nbsp;</div>'
 
 				}, {
 					xtype : 'formpanel',
 					id : 'around',
 					width : '80%',
-					height : 282,
-					margin : '5px 1px 2px 20px',
+					height : '40%',
+					margin : '5px 1px 10px 20px',
+					border:1,
 					style : "background-color:white;!important",
 					items : [{
 								xtype : 'fieldset',
@@ -29,56 +30,63 @@ Ext.define('YongYou.view.InformationConfirm', {
 								items : [{
 											xtype : 'textfield',
 											readOnly : true,
-											height : 10,
+											height : 5,
 											style : 'width:100%;font-size:16px;',
 											label : '公建配套：',
-											labelWidth:'10%',
+											labelWidth:'20%',
+											labelAlian:'right',
 											name : 'gongjian'
 										}, {
 											xtype : 'textfield',
 											readOnly : true,
-											height : 10,
-											labelWidth:'10%',
+											height : 5,
+											labelWidth:'20%',
+											labelAlian:'right',
 											style : 'width:100%;font-size:16px;',
 											label : '标志建筑：',
 											name : 'biaozhi'
 										}, {
 											xtype : 'textfield',
 											readOnly : true,
-											height : 10,
-											labelWidth:'10%',
+											height : 5,
+											labelWidth:'20%',
+											labelAlian:'right',
 											style : 'width:100%;font-size:16px;',
 											label : '公园绿化：',
 											name : 'gongyuan'
 										}, {
 											xtype : 'textfield',
 											readOnly : true,
-											height : 10,
-											labelWidth:'10%',
+											height : 5,
+											labelWidth:'20%',
+											labelAlian:'right',
 											style : 'width:100%;font-size:16px;',
 											label : '自然景观：',
 											name : 'jingguan'
 										}, {
 											xtype : 'textfield',
 											readOnly : true,
-											height : 10,
-											labelWidth:'10%',
+											height : 5,
+											labelWidth:'20%',
+											labelAlian:'right',
 											style : 'width:100%;font-size:16px;',
 											label : '就医条件：',
 											name : 'jiuyi'
 										}, {
 											xtype : 'textfield',
 											readOnly : true,
-											height : 10,
-											labelWidth:'10%',
+											height : 5,
+											labelWidth:'20%',
+											labelAlian:'right',
 											style : 'width:100%;font-size:16px;',
 											label : '就学条件：',
 											name : 'jiuxue'
 										}, {
 											xtype : 'textareafield',
 											readOnly : true,
-											height : 10,
-											labelWidth:'10%',
+											height : 5,
+											labelWidth:'20%',
+											labelAlian:'right',
 											style : 'width:100%;font-size:16px;',
 											label : '商业环境：',
 											name : 'shangye'
@@ -89,16 +97,16 @@ Ext.define('YongYou.view.InformationConfirm', {
 					xtype : 'panel',
 					collapsed : false,
 					layout : 'fit',
-					style : 'background: url(\'resources/image/touzi_bg.gif\') no-repeat left',
-					height : 100
+					style : 'background: url(\'resources/image/touzi_bg.gif\') no-repeat left;margin:10px 0px 0px 0px;',
+					height : 110
 				}, {
 					xtype : 'panel',		
-					height : 100,
+					height : 90,
 					style : 'width:100%;font-size:16px;margin-left:80px;'
 				}, {
-					xtype : 'container',
+					xtype : 'panel',
 					docked : 'bottom',
-					style : 'margin:20px;',
+					style : 'margin:10px',
 					items : [{
 						xtype : 'button',
 						ui : 'round',
@@ -128,7 +136,8 @@ Ext.define('YongYou.view.InformationConfirm', {
 						+ '</div>'
 						+ '<div class="address">&nbsp;地址:'
 						+ address
-						+ '</div></div>')
+						+ '</div>' 
+						+ '<div class="title">&nbsp;在您选取的位置方圆3公里配套情况如下：</div></div>')
 	},
 	setAroundValue : function(values) {
 		this.items.items[2].setValues(values);
